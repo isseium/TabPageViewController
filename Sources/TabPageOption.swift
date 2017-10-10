cdf3cfb21fb2e433ac55ea2b1479442d749d30a8
isseium <issei@ikmz.net>
2017-10-10 19:37:28 +0900

    add itemLabelBackgroundView

--- Sources/TabPageOption.swift
+++ Sources/TabPageOption.swift
@@ -21,7 +21,9 @@ public struct TabPageOption {
 
     public var fontSize = UIFont.systemFontSize
     public var currentColor = UIColor(red: 105/255, green: 182/255, blue: 245/255, alpha: 1.0)
+    public var currentBackgroundColor: UIColor = .clear
     public var defaultColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1.0)
+    public var defaultBackgroundColor: UIColor = .clear
     public var tabHeight: CGFloat = 32.0
     public var tabMargin: CGFloat = 20.0
     public var tabWidth: CGFloat?
